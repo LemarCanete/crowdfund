0ff9ba5f2d5a9b5fd3c74a04f07d0f49ebd7ff41
jusiemlee143 <jusiemleepontejon@gmail.com>
2024-07-25 19:15:42 +0800

    [LandingPage][JusiemLee]-Update page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -71,7 +71,7 @@ export default function Home() {
                     <div className="p-6 bg-white rounded shadow-md">
                         <img src="/member4.jpg" alt="Team Member 4" className="w-32 h-32 mx-auto rounded-full mb-4"/>
                         <h3 className="text-xl font-bold">Cyrel Joy <br />Sasan</h3>
-                        <p className="mt-2">Frontend Developer</p>
+                        <p className="mt-2">Frontend & Asst. Backend Developer</p>
                     </div>
                     <div className="p-6 bg-white rounded shadow-md">
                         <img src="/member5.jpg" alt="Team Member 5" className="w-32 h-32 mx-auto rounded-full mb-4"/>
